692227ca6773e1fc1fb253c7a318418a42c9964c
Rodrigo Stuqui Monzani <rst.monzani@gmail.com>
2024-04-12 09:47:54 -0400

    perf(fastapi): adiciona async as rotas

--- questao3/main.py
+++ questao3/main.py
@@ -10,21 +10,21 @@ db = ContaCorrenteHandler()
 
 
 @app.post("/contas/", status_code=status.HTTP_201_CREATED)
-def cadastra_conta(conta: ContaDTO):
+async def cadastra_conta(conta: ContaDTO):
     id = db.criar(conta)
     return {"id": id}
 
 
 @app.put("/contas/{id}/credito")
-def creditar_conta(id: int, valor: float):
+async def creditar_conta(id: int, valor: float):
     db.saque(id, valor)
 
 
 @app.put("/contas/{id}/debito")
-def debitar_conta(id: int, valor: float):
+async def debitar_conta(id: int, valor: float):
     db.depositar(id, valor)
 
 
 @app.get("/contas/{id}/saldo")
-def consulta_saldo_conta(id: int):
+async def consulta_saldo_conta(id: int):
     return {"valor": db.busca_saldo(id)}
